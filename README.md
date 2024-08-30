5f58cfb24a6b39bb49f9f2598fc3186ac3584454
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-08-30 18:26:24 +0200

    Plan releases
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- README.md
+++ README.md
@@ -16,7 +16,60 @@
 | &nbsp;&nbsp;[stable2407-1](https://github.com/paritytech/polkadot-sdk/releases/tag/polkadot-stable2407-1) | 2024-08-14 | 2024-08-15 |  | Released |
 | &nbsp;&nbsp;stable2407-2 | 2024-08-28 | ~2024-09-02 |  | Planned |
 | &nbsp;&nbsp;stable2407-3 | ~2024-09-11 | ~2024-09-16 |  | Planned |
-| **stable2410** | ~2024-09-02 | ~2024-09-25 | ~2025-09-25 | Planned |
+| **stable2409** | ~2024-09-02 | ~2024-09-25 | ~2025-09-25 | Planned |
+| &nbsp;&nbsp;stable2409-1 | ~2024-10-09 | ~2024-10-12 |  | Planned |
+| &nbsp;&nbsp;stable2409-2 | ~2024-10-23 | ~2024-10-26 |  | Planned |
+| &nbsp;&nbsp;stable2409-3 | ~2024-11-06 | ~2024-11-09 |  | Planned |
+| &nbsp;&nbsp;stable2409-4 | ~2024-11-20 | ~2024-11-23 |  | Planned |
+| &nbsp;&nbsp;stable2409-5 | ~2024-12-04 | ~2024-12-07 |  | Planned |
+| &nbsp;&nbsp;stable2409-6 | ~2024-12-18 | ~2024-12-21 |  | Planned |
+| &nbsp;&nbsp;stable2409-7 | ~2025-01-01 | ~2025-01-04 |  | Planned |
+| &nbsp;&nbsp;stable2409-8 | ~2025-01-15 | ~2025-01-18 |  | Planned |
+| &nbsp;&nbsp;stable2409-9 | ~2025-01-29 | ~2025-02-01 |  | Planned |
+| &nbsp;&nbsp;stable2409-10 | ~2025-02-12 | ~2025-02-15 |  | Planned |
+| &nbsp;&nbsp;stable2409-11 | ~2025-02-26 | ~2025-03-01 |  | Planned |
+| &nbsp;&nbsp;stable2409-12 | ~2025-03-12 | ~2025-03-15 |  | Planned |
+| &nbsp;&nbsp;stable2409-13 | ~2025-03-26 | ~2025-03-29 |  | Planned |
+| &nbsp;&nbsp;stable2409-14 | ~2025-04-09 | ~2025-04-12 |  | Planned |
+| &nbsp;&nbsp;stable2409-15 | ~2025-04-23 | ~2025-04-26 |  | Planned |
+| &nbsp;&nbsp;stable2409-16 | ~2025-05-07 | ~2025-05-10 |  | Planned |
+| &nbsp;&nbsp;stable2409-17 | ~2025-05-21 | ~2025-05-24 |  | Planned |
+| &nbsp;&nbsp;stable2409-18 | ~2025-06-04 | ~2025-06-07 |  | Planned |
+| &nbsp;&nbsp;stable2409-19 | ~2025-06-18 | ~2025-06-21 |  | Planned |
+| &nbsp;&nbsp;stable2409-20 | ~2025-07-02 | ~2025-07-05 |  | Planned |
+| &nbsp;&nbsp;stable2409-21 | ~2025-07-16 | ~2025-07-19 |  | Planned |
+| &nbsp;&nbsp;stable2409-22 | ~2025-07-30 | ~2025-08-02 |  | Planned |
+| &nbsp;&nbsp;stable2409-23 | ~2025-08-13 | ~2025-08-16 |  | Planned |
+| &nbsp;&nbsp;stable2409-24 | ~2025-08-27 | ~2025-08-30 |  | Planned |
+| &nbsp;&nbsp;stable2409-25 | ~2025-09-10 | ~2025-09-13 |  | Planned |
+| &nbsp;&nbsp;stable2409-26 | ~2025-09-24 | ~2025-09-27 |  | Planned |
+| **stable2501** | ~2025-01-01 | ~2025-02-15 | ~2026-02-15 | Planned |
+| &nbsp;&nbsp;stable2501-1 | ~2025-03-01 | ~2025-03-04 |  | Planned |
+| &nbsp;&nbsp;stable2501-2 | ~2025-03-15 | ~2025-03-18 |  | Planned |
+| &nbsp;&nbsp;stable2501-3 | ~2025-03-29 | ~2025-04-01 |  | Planned |
+| &nbsp;&nbsp;stable2501-4 | ~2025-04-12 | ~2025-04-15 |  | Planned |
+| &nbsp;&nbsp;stable2501-5 | ~2025-04-26 | ~2025-04-29 |  | Planned |
+| &nbsp;&nbsp;stable2501-6 | ~2025-05-10 | ~2025-05-13 |  | Planned |
+| &nbsp;&nbsp;stable2501-7 | ~2025-05-24 | ~2025-05-27 |  | Planned |
+| &nbsp;&nbsp;stable2501-8 | ~2025-06-07 | ~2025-06-10 |  | Planned |
+| &nbsp;&nbsp;stable2501-9 | ~2025-06-21 | ~2025-06-24 |  | Planned |
+| &nbsp;&nbsp;stable2501-10 | ~2025-07-05 | ~2025-07-08 |  | Planned |
+| &nbsp;&nbsp;stable2501-11 | ~2025-07-19 | ~2025-07-22 |  | Planned |
+| &nbsp;&nbsp;stable2501-12 | ~2025-08-02 | ~2025-08-05 |  | Planned |
+| &nbsp;&nbsp;stable2501-13 | ~2025-08-16 | ~2025-08-19 |  | Planned |
+| &nbsp;&nbsp;stable2501-14 | ~2025-08-30 | ~2025-09-02 |  | Planned |
+| &nbsp;&nbsp;stable2501-15 | ~2025-09-13 | ~2025-09-16 |  | Planned |
+| &nbsp;&nbsp;stable2501-16 | ~2025-09-27 | ~2025-09-30 |  | Planned |
+| &nbsp;&nbsp;stable2501-17 | ~2025-10-11 | ~2025-10-14 |  | Planned |
+| &nbsp;&nbsp;stable2501-18 | ~2025-10-25 | ~2025-10-28 |  | Planned |
+| &nbsp;&nbsp;stable2501-19 | ~2025-11-08 | ~2025-11-11 |  | Planned |
+| &nbsp;&nbsp;stable2501-20 | ~2025-11-22 | ~2025-11-25 |  | Planned |
+| &nbsp;&nbsp;stable2501-21 | ~2025-12-06 | ~2025-12-09 |  | Planned |
+| &nbsp;&nbsp;stable2501-22 | ~2025-12-20 | ~2025-12-23 |  | Planned |
+| &nbsp;&nbsp;stable2501-23 | ~2026-01-03 | ~2026-01-06 |  | Planned |
+| &nbsp;&nbsp;stable2501-24 | ~2026-01-17 | ~2026-01-20 |  | Planned |
+| &nbsp;&nbsp;stable2501-25 | ~2026-01-31 | ~2026-02-03 |  | Planned |
+| &nbsp;&nbsp;stable2501-26 | ~2026-02-14 | ~2026-02-17 |  | Planned |
 
 
 <!-- TEMPLATE END -->
---
+++ manage.py
@@ -0,0 +1,256 @@
+import argparse
+import json
+from datetime import datetime, timedelta
+import sys
+import re
+
+def load_json(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
+def save_json(data, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=2)
+
+def validate_version(version):
+    if not re.match(r'^stable2[45][01][0-9](-[0-9]*)?$', version):
+        raise ValueError("Invalid version format. Expected 'stableYYMM' or 'stableYYMM-X' for patches.")
+
+def validate_date(date_str):
+    try:
+        return datetime.strptime(date_str, '%Y-%m-%d').strftime('%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Invalid date format. Expected 'YYYY-MM-DD'.")
+
+def find_release(data, version):
+    release_version = version.split('-')[0]
+    for project in data.values():
+        for release in project['releases']:
+            if release['name'] == release_version:
+                return project, release
+    return None, None
+
+def update_patch(release, patch_number, date, field):
+    if 'patches' not in release:
+        release['patches'] = []
+
+    for patch in release['patches']:
+        if patch['name'].endswith(f'-{patch_number}'):
+            if field == 'cutoff':
+                patch['cutoff'] = date
+                patch['state'] = 'testing'
+            elif field == 'publish':
+                patch['publish'] = {'when': date, 'tag': f'polkadot-{patch["name"]}'}
+                patch['state'] = 'released'
+                # Also set the cutoff if it was not set
+                if isinstance(patch['cutoff'], dict) and 'estimated' in patch['cutoff']:
+                    patch['cutoff'] = date
+            elif field == 'plan':
+                patch['cutoff'] = {'estimated': date}
+                patch['publish'] = {'estimated': date}
+                patch['state'] = 'planned'
+            return True
+
+    # If the patch doesn't exist, create it
+    new_patch = {
+        'name': f'{release["name"]}-{patch_number}',
+        'cutoff': date if field == 'cutoff' else {'estimated': date},
+        'publish': {'when': date, 'tag': f'polkadot-{release["name"]}-{patch_number}'} if field == 'publish' else {'estimated': date},
+        'state': 'testing' if field == 'cutoff' else ('released' if field == 'publish' else 'planned')
+    }
+    release['patches'].append(new_patch)
+    return True
+
+def create_planned_patches(release, start_date, num_patches=26):
+    if 'patches' not in release:
+        release['patches'] = []
+
+    existing_patch_numbers = set(int(patch['name'].split('-')[-1]) for patch in release['patches'] if '-' in patch['name'])
+    
+    last_patch_date = datetime.strptime(start_date, '%Y-%m-%d')
+    for i in range(1, num_patches + 1):
+        if i not in existing_patch_numbers:
+            cutoff_date = (last_patch_date + timedelta(days=14)).strftime('%Y-%m-%d')
+            publish_date = (last_patch_date + timedelta(days=17)).strftime('%Y-%m-%d')
+            new_patch = {
+                'name': f'{release["name"]}-{i}',
+                'cutoff': {'estimated': cutoff_date},
+                'publish': {'estimated': publish_date},
+                'state': 'planned'
+            }
+            release['patches'].append(new_patch)
+            last_patch_date = datetime.strptime(cutoff_date, '%Y-%m-%d')
+        else:
+            # If the patch already exists, find its date to continue the sequence
+            existing_patch = next(patch for patch in release['patches'] if patch['name'].endswith(f'-{i}'))
+            if 'cutoff' in existing_patch:
+                # If no publish date, use cutoff date
+                if isinstance(existing_patch['cutoff'], dict):
+                    if 'when' in existing_patch['cutoff']:
+                        last_patch_date = datetime.strptime(existing_patch['cutoff']['when'], '%Y-%m-%d')
+                    elif 'estimated' in existing_patch['cutoff']:
+                        last_patch_date = datetime.strptime(existing_patch['cutoff']['estimated'], '%Y-%m-%d')
+                elif isinstance(existing_patch['cutoff'], str):
+                    last_patch_date = datetime.strptime(existing_patch['cutoff'], '%Y-%m-%d')
+            else:
+                # If no publish or cutoff date, use the previous patch date + 14 days
+                last_patch_date += timedelta(days=14)
+
+    # Sort patches by their number to maintain order
+    release['patches'].sort(key=lambda x: int(x['name'].split('-')[-1]))
+
+def update_release(data, version, date, field):
+    project, release = find_release(data, version)
+
+    if not release and field == 'plan':
+        # Create a new release if it doesn't exist
+        project = next(iter(data.values()))  # Get the first (and only) project
+        cutoff_date = datetime.strptime(date, '%Y-%m-%d')
+        publish_date = cutoff_date + timedelta(days=45)  # 1.5 months after cutoff
+        new_release = {
+            'name': version,
+            'cutoff': {'estimated': date},
+            'publish': {'estimated': publish_date.strftime('%Y-%m-%d')},
+            'state': 'planned',
+            'endOfLife': {'estimated': (publish_date + timedelta(days=365)).strftime('%Y-%m-%d')}
+        }
+        project['releases'].append(new_release)
+        create_planned_patches(new_release, publish_date.strftime('%Y-%m-%d'))
+        return True
+
+    if not release:
+        return False
+
+    if '-' in version:  # It's a patch
+        return update_patch(release, version.split('-')[1], date, field)
+    else:  # It's a release
+        if field == 'cutoff':
+            release['cutoff'] = date
+            release['state'] = 'testing'
+        elif field == 'publish':
+            release['publish'] = {'when': date, 'tag': f'polkadot-{version}'}
+            release['state'] = 'released'
+        elif field == 'plan':
+            cutoff_date = datetime.strptime(date, '%Y-%m-%d')
+            publish_date = cutoff_date + timedelta(days=45)  # 1.5 months after cutoff
+            release['cutoff'] = {'estimated': date}
+            release['publish'] = {'estimated': publish_date.strftime('%Y-%m-%d')}
+            release['state'] = 'planned'
+            create_planned_patches(release, publish_date.strftime('%Y-%m-%d'))
+        return True
+
+def deprecate_release(data, version, date, use_instead):
+    project, release = find_release(data, version)
+    if not release:
+        return False
+
+    release['state'] = {
+        'deprecated': {
+            'since': date,
+            'useInstead': use_instead
+        }
+    }
+    return True
+
+def backfill_patches(data, version=None):
+    releases_updated = False
+    for project in data.values():
+        for release in project['releases']:
+            if release['name'].startswith('stable') and (version is None or release['name'] == version):
+                if 'publish' in release and isinstance(release['publish'], dict):
+                    if 'when' in release['publish']:
+                        start_date = release['publish']['when']
+                    elif 'estimated' in release['publish']:
+                        start_date = release['publish']['estimated']
+                    else:
+                        continue  # Skip if no valid publish date
+                else:
+                    continue  # Skip if no valid publish field
+
+                create_planned_patches(release, start_date)
+                releases_updated = True
+                
+                if version:  # If a specific version was requested, we're done after processing it
+                    return True
+    return releases_updated
+
+def handle_release_command(args, data):
+    validate_version(args.version)
+    date = validate_date(args.date)
+    if update_release(data, args.version, date, args.field):
+        save_json(data, args.file)
+        print(f"Successfully updated {args.field} for {args.version}")
+    else:
+        print(f"Release or patch {args.version} not found")
+
+def handle_deprecate_command(args, data):
+    validate_version(args.version)
+    date = validate_date(args.date)
+    validate_version(args.use_instead)
+    if deprecate_release(data, args.version, date, args.use_instead):
+        save_json(data, args.file)
+        print(f"Successfully deprecated {args.version}")
+    else:
+        print(f"Release {args.version} not found")
+
+def handle_backfill_patches_command(args, data):
+    if args.version:
+        validate_version(args.version)
+        if backfill_patches(data, args.version):
+            save_json(data, args.file)
+            print(f"Successfully backfilled patches for {args.version}")
+        else:
+            print(f"Failed to backfill patches for {args.version}. Release may not exist or have a valid publish date.")
+    else:
+        if backfill_patches(data):
+            save_json(data, args.file)
+            print("Successfully backfilled patches for all applicable stable releases")
+        else:
+            print("No releases were updated. All releases may already have patches or lack valid publish dates.")
+
+def main():
+    parser = argparse.ArgumentParser(description="Manage releases-v1.json file")
+    subparsers = parser.add_subparsers(dest='action', required=True)
+
+    # Release parser
+    release_parser = subparsers.add_parser('release')
+    release_parser.add_argument('field', choices=['cutoff', 'publish', 'plan'])
+    release_parser.add_argument('version', help="Release version (e.g., stable2401 or stable2401-1 for patches)")
+    release_parser.add_argument('date', help="Date in YYYY-MM-DD format")
+
+    # Deprecate parser
+    deprecate_parser = subparsers.add_parser('deprecate')
+    deprecate_parser.add_argument('version', help="Release version to deprecate")
+    deprecate_parser.add_argument('date', help="Deprecation date in YYYY-MM-DD format")
+    deprecate_parser.add_argument('use_instead', help="Version to use instead")
+
+    # Backfill patches parser
+    backfill_parser = subparsers.add_parser('backfill-patches')
+    backfill_parser.add_argument('version', nargs='?', help="Specific stable version to backfill (e.g., stable2401)")
+
+    parser.add_argument('--file', default='releases-v1.json', help="Path to the JSON file")
+
+    args = parser.parse_args()
+
+    try:
+        data = load_json(args.file)
+
+        if args.action == 'release':
+            handle_release_command(args, data)
+        elif args.action == 'deprecate':
+            handle_deprecate_command(args, data)
+        elif args.action == 'backfill-patches':
+            handle_backfill_patches_command(args, data)
+
+    except ValueError as e:
+        print(f"Error: {str(e)}")
+        sys.exit(1)
+    except FileNotFoundError:
+        print(f"Error: File '{args.file}' not found")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print(f"Error: Invalid JSON in '{args.file}'")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
